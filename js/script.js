69fbd287e80352e941a2d7715d1634c6227ec080
lorenzoramong <lorenzoramong@gmail.com>
2025-11-04 22:47:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+async function cargarUsuarios() {
+  const response = await fetch("../data/users.json");
+  return await response.json();
+}
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const message = document.getElementById("loginMessage");
+
+  const usuarios = await cargarUsuarios();
+  const usuario = usuarios.find(u => u.usuario === username && u.contraseña === password);
+
+  if (usuario) {
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+    message.textContent = "Bienvenido " + usuario.nombre + "!";
+    message.style.color = "green";
+    setTimeout(() => {
+      if (usuario.rol === "super_admin" || usuario.rol === "admin") {
+        window.location.href = "dashboard.html";
+      } else {
+        window.location.href = "tareas.html";
+      }
+    }, 800);
+  } else {
+    message.textContent = "Usuario o contraseña incorrectos.";
+    message.style.color = "red";
+  }
+});
 
